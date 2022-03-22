1177f55c916a1d8b15bc73ec69eb5c81ab6447e2
DHG18 <54952634+DHG18@users.noreply.github.com>
2022-03-22 13:55:11 -0400

    Update IEscrow.sol

--- contracts/interfaces/IEscrow.sol
+++ contracts/interfaces/IEscrow.sol
@@ -4,6 +4,5 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IEscrow is IModule {
 
-  function healthcheck() external returns (LoanLib.STATUS status);
   function releaseCollateral(IERC20 token, uint256 _amount, address arbiter) external;
 }
