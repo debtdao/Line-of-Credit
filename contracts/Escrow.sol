86c5f1326ce3270267b82bee962f9dbe53a5d59d
James Sangalli <j.l.sangalli@gmail.com>
2022-03-28 10:30:12 +1100

    WIP implement liquidate function #7

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -8,12 +8,13 @@ import { IOracle } from "./interfaces/IOracle.sol";
 contract Escrow is IEscrow {
 
     uint public minimumCollateralRatio; // the minimum value of the collateral in relation to the outstanding debt e.g. 10% of outstanding debt
-    address public loanContract;
+    address public loan;
     address public oracle; // used to value assets, must be an approved source
     address public lender;
     address public borrower;
+    address public arbiter;
     bool public initCalled = false;
-    LoanLib.STATUS currentStatus;
+    LoanLib.STATUS public lastUpdatedStatus; // this status can constantly change, hence last updated status
     mapping(address => uint) public deposited; // tokens used as collateral (must be able to value with oracle)
     mapping(address => Farm) public farmedTokens; // collateral tokens that have been used for farming
 
@@ -22,34 +23,36 @@ contract Escrow is IEscrow {
         address _loanContract,
         address _oracle,
         address _lender,
-        address _borrower
+        address _borrower,
+        address _arbiter
     ) public {
         minimumCollateralRatio = minimumCollateralRatio;
-        loanContract = loanContract;
+        loan = loan;
         oracle = _oracle;
         lender = _lender;
         borrower = _borrower;
-        currentStatus = LoanLib.STATUS.UNINITIALIZED; // TODO at what point does the escrow become initialized?
+        arbiter = _arbiter;
+        lastUpdatedStatus = LoanLib.STATUS.UNINITIALIZED; // TODO at what point does the escrow become initialized?
     }
 
     function init() external {
         // TODO
         require(!initCalled, "Escrow: init() has already been called");
-        currentStatus = LoanLib.STATUS.INITIALIZED;
+        lastUpdatedStatus = LoanLib.STATUS.INITIALIZED;
         initCalled = true;
     }
 
     /*
     * @dev see IModule.sol
     */
-    function healthcheck() external returns (LoanLib.STATUS status) {
+    function healthcheck() public returns (LoanLib.STATUS status) {
         revert("Not implemented");
     }
 
     /*
-    * @dev updates the current health status by checking cratio
+    * @dev updates the current status by checking the cratio with the latest information
     */
-    function _updateHealthCheck() internal {
+    function _updateCollateralRatio() internal {
         revert("Not implemented");
     }
 
@@ -57,7 +60,7 @@ contract Escrow is IEscrow {
     * @dev see IModule.sol
     */
     function loan() external returns (address) {
-        return loanContract;
+        return loan;
     }
 
     /*
@@ -70,14 +73,7 @@ contract Escrow is IEscrow {
         );
         require(IERC20(token).transferFrom(msg.sender, address(this), amount));
         deposited[token] += amount;
-        _updateHealthCheck();
-    }
-
-    /*
-    * @dev see IEscrow.sol
-    */
-    function getCollateralRatio() public returns(uint) {
-        revert("Not implemented");
+        _updateCollateralRatio();
     }
 
     /*
@@ -90,8 +86,18 @@ contract Escrow is IEscrow {
     /*
     * @dev see IEscrow.sol
     */
-    function liquidate(address token, uint amount) public {
+    function getCollateralRatio() public returns(uint) {
         revert("Not implemented");
+    }
+
+    /*
+    * @dev see IEscrow.sol
+    */
+    function liquidate(address token, uint amount) public {
+        require(msg.sender == loan, "Escrow: msg.sender must be the loan contract");
+        require(healthcheck() == LoanLib.STATUS.LIQUIDATABLE, "Escrow: not eligible for liquidation");
+        require(IERC20(token).transferFrom(address(this), arbiter, amount));
+        deposited[token] -= amount;
     }
 
     // TODO @smokey
