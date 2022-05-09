0b5d906bc36de5977d82baf4e0164aaef06f47b5
DHG18 <54952634+DHG18@users.noreply.github.com>
2022-05-09 04:41:29 -0400

    Added oracle contract and updated IOracle interface (#28)
    * Oracle contract + interface (returns price scaled for decimals in USD)
    
    * Removed scaling within oracle contract
    
    Co-authored-by: DHG18 <dahg18@gmail.com>

---
+++ contracts/modules/oracle/Oracle.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@chainlink/contracts/src/v0.8/interfaces/FeedRegistryInterface.sol";
+import "../../interfaces/IOracle.sol";
+
+contract Oracle is IOracle {
+    FeedRegistryInterface internal registry;
+    address public USD = 0x0000000000000000000000000000000000000348;
+
+    constructor(address _registry) {
+        registry = FeedRegistryInterface(_registry);
+    }
+
+    /**
+     * Returns the latest price in USD
+     */
+    function getLatestAnswer(address token) external view returns (int) {
+        (
+            /* uint80 roundID */, 
+            int price,
+            /* uint80 startedAt */,
+            /* uint80 timeStamp */,
+            /* uint80 answeredInRound */
+        ) = registry.latestRoundData(token, USD); // ALL PRICES WILL HAVE 8 DECIMAL PADDING
+        
+        return price;
+    }
+
+    
+}
