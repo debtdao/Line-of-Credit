d6c7d99d06333244143e4220438f208db25afafc
Kiba Gateaux <kibagateaux@gmail.com>
2022-05-26 13:25:01 +0900

    Add/spigot features (#83)
    * update spigot consumer integration with loan contracts
    
    * update spigot consumer claimAndTrade functionality
    
    * allow spigot owner split to be updated and add function on spigot consumer to modifyit
    
    * roll SpigotConsumer into SpigotedLoan to simplify
    
    * update spigot sweep funcrtionality

--- contracts/interfaces/ISpigotedLoan.sol
+++ contracts/interfaces/ISpigotedLoan.sol
@@ -1,13 +1,29 @@
 pragma solidity ^0.8.9;
 interface ISpigotedLoan {
   event RevenuePayment(
-    address indexed revToken,
-    uint256 indexed revValue
+    address indexed token,
+    uint256 indexed amount,
+    uint256 indexed value
   );
 
-  function claimSpigotAndRepay(
+  event TradeSpigotRevenue(
+    address indexed revenueToken,
+    uint256 revenueTokenAmount,
+    address indexed debtToken,
+    uint256 indexed debtTokensBought
+  );
+  
+  function claimAndTrade(
+    address claimToken, 
+    address targetToken,
+    bytes calldata zeroExTradeData
+  ) external returns(uint256 tokensBought);
+
+  function claimAndRepay(
     bytes32 positionId,
     address token,
     bytes calldata zeroExTradeData
   ) external returns(bool);
+
+  function sweep(address token) external returns(bool);
 }
