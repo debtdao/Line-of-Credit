232512dd997bd8f4aec518aa5cd954fbfeffe537
cantbeevil <wontbeevil@protonmail.com>
2022-08-25 19:07:41 -0700

    Update InterestRate tests to Forge

--- contracts/tests/InterestRateCredit.t.sol
+++ contracts/tests/InterestRateCredit.t.sol
@@ -1,45 +1,81 @@
-
 pragma solidity ^0.8.9;
 
+import "forge-std/Test.sol";
 
-import {DSTest} from "../../lib/ds-test/src/test.sol";
+import {InterestRateCredit} from "../modules/interest-rate/InterestRateCredit.sol";
 
-import { InterestRateCredit } from  "../modules/interest-rate/InterestRateCredit.sol";
-
-/**
- * @notice
- * @dev - does not test spigot integration e.g. claimEscrow() since that should already be covered in Spigot tests
- *      - these tests would fail if that assumption was wrong anyway
- */
-contract SpigotedLineTest is DSTest {
+contract InterestRateCreditTest is Test {
     InterestRateCredit i;
 
     function setUp() public {
-      i = new InterestRateCredit();
+        i = new InterestRateCredit();
+        vm.warp(0);
     }
 
     function test_can_add_different_rates() public {
-      i.setRate(bytes32(""), uint128(0), uint128(0));
-      i.setRate(bytes32("1"), uint128(1), uint128(1));
+        i.setRate(bytes32(""), uint128(0), uint128(0));
+        (uint128 d, uint128 f, uint256 l) = i.rates(bytes32(""));
+        assertEq(d, 0);
+        assertEq(f, 0);
+        assertEq(l, block.timestamp);
+        i.setRate(bytes32("1"), uint128(1), uint128(1));
+        (d, f, l) = i.rates(bytes32("1"));
+        assertEq(d, 1);
+        assertEq(f, 1);
+        assertEq(l, block.timestamp);
     }
 
-    function test_can_accrue_interest_all_drawn() public {
-      i.setRate(bytes32(""), uint128(0), uint128(0));
-      uint accrued = i.accrueInterest(bytes32(0), 1, 1);
-      assertEq(accrued, 0); // TODO: figure out how to fast forward blocks in hevm to test real amounts
+    function test_can_accrue_interest_all_drawn(
+        uint128 drawnRate,
+        uint64 drawnBalance
+    ) public {
+        vm.assume(drawnRate > 0 && drawnRate <= 1e4);
+        vm.assume(drawnBalance >= 1e4);
+        bytes32 id = bytes32("");
+        i.setRate(id, drawnRate, uint128(3));
+        skip(365.25 days);
+        uint256 accrued = i.accrueInterest(id, drawnBalance, drawnBalance);
+        assertEq(accrued, (drawnRate * drawnBalance) / 1e4);
     }
 
+    function test_accrue_interest_drawn_half_drawn(uint200 balance) public {
+        vm.assume(balance >= 2e4);
 
-    function test_can_accrue_interest_half_drawn() public {
-      i.setRate(bytes32(""), uint128(0), uint128(0));
-      uint accrued = i.accrueInterest(bytes32(0), 1, 2);
-      assertEq(accrued, 0); // TODO: figure out how to fast forward blocks in hevm to test real amounts
+        bytes32 id = bytes32("");
+        uint128 drawnRate = 603;
+        uint128 facilityRate = 118;
+        uint256 drawnBalance = balance / 2;
+        uint256 facilityBalance = balance;
+
+        i.setRate(id, drawnRate, facilityRate);
+        skip(365.25 days);
+        uint256 accrued = i.accrueInterest(id, drawnBalance, facilityBalance);
+
+        assertEq(
+            accrued,
+            (((drawnRate * drawnBalance) / 1e4) +
+                ((facilityRate * (facilityBalance - drawnBalance)) / 1e4))
+        );
     }
 
+    function test_can_accrue_interest_none_drawn(uint200 balance) public {
+        bytes32 id = bytes32("");
+        uint128 facilityRate = 71;
+        uint256 facilityBalance = balance;
+        i.setRate(id, uint128(3), uint128(facilityRate));
+        skip(365.25 days);
+        uint256 accrued = i.accrueInterest(id, 0, facilityBalance);
+        assertEq(accrued, (facilityRate * facilityBalance) / 1e4);
+    }
 
-    function test_can_accrue_interest_none_drawn() public {
-      i.setRate(bytes32(""), uint128(0), uint128(0));
-      uint accrued = i.accrueInterest(bytes32(0), 0, 1);
-      assertEq(accrued, 0); // TODO: figure out how to fast forward blocks in hevm to test real amounts
+    function test_lastAccrued_update() public {
+        uint256 prevBlocktime = block.timestamp;
+        uint256 timeToSkip = 98381;
+        bytes32 id = bytes32("");
+        i.setRate(id, uint128(0), uint128(0));
+        skip(timeToSkip);
+        i.accrueInterest(bytes32(0), 0, 1);
+        (, , uint256 lastAccrued) = i.rates(id);
+        assertEq(lastAccrued, prevBlocktime + timeToSkip);
     }
 }
