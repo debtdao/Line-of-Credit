96983147673c97d7820c59f31220f380777bb430
Momin Ahmad <mominahmad93@gmail.com>
2022-08-31 15:50:33 -0500

    minor tweak to LoC

--- contracts/modules/credit/LineOfCredit.sol
+++ contracts/modules/credit/LineOfCredit.sol
@@ -179,11 +179,11 @@ contract LineOfCredit is ILineOfCredit, MutualConsent {
         address oracle_ = address(oracle);  // gas savings
         address interestRate_ = address(interestRate);
         
-        for (uint256 i; i < len;) {
+        for (uint256 i; i < len; ++i) {
             id = ids[i];
 
             // gas savings. capped to len. inc before early continue
-            unchecked { ++i; }
+            
 
             // null element in array
             if(id == bytes32(0)) { continue; }
@@ -209,11 +209,11 @@ contract LineOfCredit is ILineOfCredit, MutualConsent {
     function accrueInterest() external override returns(bool) {
         uint256 len = ids.length;
         bytes32 id;
-        for (uint256 i; i < len;) {
+        for (uint256 i; i < len; ++i) {
           id = ids[i];
           Credit memory credit = credits[id];
           credits[id] = _accrue(credit, id);
-          unchecked { ++i; }
+          
         }
         
         return true;
