40500e36f11f810e0b00ad830704087ff0bce253
Momin Ahmad <mominahmad93@gmail.com>
2022-10-11 19:44:45 -0500

    attempting to fund loan

--- contracts/scripts/ApprovalScript.s.sol
+++ contracts/scripts/ApprovalScript.s.sol
@@ -14,13 +14,18 @@ contract ApprovalScript is Script {
     address kiba = address(0xDe8f0F6769284e41Bf0f82d0545141c15A3E4aD1);
     address sero = address(0x1A6784925814a13334190Fd249ae0333B90b6443);
     address mo = address(0xf44B95991CaDD73ed769454A03b3820997f00873);
+
     uint constant MAX_INT = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
 
      function run() public {
-        uint256 deployerPrivateKey= vm.envUint("PRIVATE_KEY");
+        // uint256 deployerPrivateKey= vm.envUint("PRIVATE_KEY");
+        // vm.startBroadcast(deployerPrivateKey);
+
+        uint256 deployerPrivateKey= vm.envUint("CRAIG_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
         line = SecuredLine(payable(0xE88790286AfD3DE0Ae0144706476287e2Cd08874));
+        address line_add = address(line);
         address escrow_add = address(line.escrow());
  
 
@@ -28,8 +33,13 @@ contract ApprovalScript is Script {
         kiibaCoin = RevToken(address(0x3D4AA21e8915F3b5409BDb20f76457FCdAF8f757));
         seeroCoin = RevToken(address(0x3730954eC1b5c59246C1fA6a20dD6dE6Ef23aEa6));
 
-        mooCoin.approve(address(escrow_add), MAX_INT);
-        kiibaCoin.approve(address(escrow_add), MAX_INT);
-        seeroCoin.approve(address(escrow_add), MAX_INT);
+        // mooCoin.approve(address(escrow_add), MAX_INT);
+        // kiibaCoin.approve(address(escrow_add), MAX_INT);
+        // seeroCoin.approve(address(escrow_add), MAX_INT);
+
+        mooCoin.approve(address(line_add), MAX_INT);
+        kiibaCoin.approve(address(line_add), MAX_INT);
+        seeroCoin.approve(address(line_add), MAX_INT);
+
     }
 }
--- contracts/scripts/LineScript.s.sol
+++ contracts/scripts/LineScript.s.sol
@@ -14,13 +14,16 @@ contract LineScript is Script {
         uint256 deployerPrivateKey= vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
+        // uint256 deployerPrivateKey= vm.envUint("CRAIG_KEY");
+        // vm.startBroadcast(deployerPrivateKey);
+
         line = SecuredLine(payable(0xE88790286AfD3DE0Ae0144706476287e2Cd08874));
         address escrow_add = address(line.escrow());
     
         escrow = Escrow(payable(escrow_add));
         // escrow.enableCollateral(address(0x3D4AA21e8915F3b5409BDb20f76457FCdAF8f757));
         // escrow.addCollateral(amount, address(0x3D4AA21e8915F3b5409BDb20f76457FCdAF8f757));
-        line.addCredit(2000, 1000, 2000000000 ether, address(0x3730954eC1b5c59246C1fA6a20dD6dE6Ef23aEa6), address(0x1A6784925814a13334190Fd249ae0333B90b6443));
-
+        //line.addCredit(2000, 1000, 2000000000 ether, address(0x3730954eC1b5c59246C1fA6a20dD6dE6Ef23aEa6), address(0x0980510F95F4fAB5629a497F9FeA58a1f44FC121));
+        
     }
 }
