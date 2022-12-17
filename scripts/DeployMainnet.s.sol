b930ec7a5d5c2c75f31e14941e6fc30f17f8d161
Craigson <craigpick@gmail.com>
2022-12-17 00:16:06 -0500

    add mainnet details

--- scripts/DeployMainnet.s.sol
+++ scripts/DeployMainnet.s.sol
@@ -8,14 +8,13 @@ import {Oracle} from "../contracts/modules/oracle/Oracle.sol";
 contract DeployMainnet is DeployBase {
     Oracle oracle;
 
-    address arbiter = address(0);
-    address swapTarget = address(0);
-    address feedRegistry = 0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf; // TODO: confirm this
+    address arbiter = 0xB2EB96f809A27bbA9E28a7faf8F4316414cec468; // DD
+    address swapTarget = 0xdef1c0ded9bec7f1a1670819833240f027b25eff; // 0x
+    address feedRegistry = 0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf; // Chainlink
 
     function run() public {
         console.log("Starting Mainnet deployment");
         uint256 deployerKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
-        // address deployer = vm.addr(deployerKey);
 
         vm.startBroadcast(deployerKey);
 
