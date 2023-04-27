856fe7f21abf8fc4a7395a9ea0da50318dcb9892
kibagateaux <kibagateaux@gmail.com>
2023-04-27 00:15:50 -0400

    add rsa tests

--- contracts/tests/RSA.t.sol
+++ contracts/tests/RSA.t.sol
@@ -1,9 +1,8 @@
 pragma solidity 0.8.16;
 
 import "forge-std/Test.sol";
+import {RSA} from "../modules/credit/RSA.sol";
 import {Spigot} from "../modules/spigot/Spigot.sol";
-
-import {SpigotLib} from "../utils/SpigotLib.sol";
 
 import {RevenueToken} from "../mock/RevenueToken.sol";
 import {SimpleRevenueContract} from "../mock/SimpleRevenueContract.sol";
@@ -13,10 +12,12 @@ import {ISpigot} from "../interfaces/ISpigot.sol";
 
 contract SpigotTest is Test {
     // spigot contracts/configurations to test against
-    RevenueToken private token;
+    RevenueToken private revenueToken;
+    RevenueToken private creditToken;
     address private revenueContract;
     Spigot private spigot;
     ISpigot.Setting private settings;
+    uint8 private lenderRevenueSplit = 47;
 
     // Named vars for common inputs
     uint256 constant MAX_REVENUE = type(uint256).max / 100;
@@ -29,31 +30,152 @@ contract SpigotTest is Test {
         SimpleRevenueContract.claimPullPayment.selector;
     bytes4 constant claimPushPaymentFunc = bytes4(0);
 
-    // create dynamic arrays for function args
-    // Mostly unused in tests so convenience for empty array
-    bytes4[] private whitelist;
-    address[] private c;
     ISpigot.Setting[] private s;
 
-    // Spigot Controller access control vars
-    address private owner;
+    // RSA + Spigot stakeholder
+    RSA private rsa;
     address private operator;
+    address private borrower;
+    address private lender;
   
 
     function setUp() public {
-        owner = address(this);
-        operator = address(10);
+        operator = vm.addr(2);
+        borrower = vm.addr(3);
+        lender = vm.addr(4);
         
-        token = new RevenueToken();
+        revenueToken = new RevenueToken();
+        creditToken = new RevenueToken();
 
         _initSpigot(
-            address(token),
+            address(revenueToken),
+            100,
+            claimPushPaymentFunc,
+            transferOwnerFunc
+        );
+
+        rsa = _initRSA(
+            spigot,
+            address(creditToken),
             100,
             claimPushPaymentFunc,
             transferOwnerFunc
         );
 
         // TODO find some good revenue contracts to mock and deploy
+    }
+
+
+    function test_deposit_lenderMustDepositInitialPrincipal() public {
+        uint256 balance1 = creditToken.balanceOf(lender);
+        startPrank(lender);
+        rsa.deposit();
+        uint256 balance2 = creditToken.balanceOf(lender);
+        assertEq(balance1 - balance2, rsa.initialPrincipal());
+    }
+
+    function test_deposit_borrowerGetsInitialPrincipalOnDeposit() public {
+        uint256 balance1 = creditToken.balanceOf(borrower);
+        startPrank(lender);
+        rsa.deposit();
+        uint256 balance2 = creditToken.balanceOf(borrower);
+        assertEq(balance1 - balance2, rsa.initialPrincipal());
+    }
+
+    /*********************
+    **********************
+    
+    CowSwap Market Order Creation
+    
+    **********************
+    *********************/
+
+    function test_initiateOrder_returnsOrderHash() public {
+        startPrank(lender);
+        rsa.deposit();
+
+    }
+
+    function invariant_initiateOrder_mustSellOver1Token() public {
+        startPrank(lender);
+        rsa.deposit();
+        vm.expectRevert("Invalid trade amoun");
+        rsa.initiateOrder(revenueToken, 0, 0, block.timestamp + 100 days);
+    }
+
+    function invariant_initiateOrder_cantTradeIfNoDebt() public {
+        // havent deposited so no debt
+        vm.expectRevert("Trade not require");
+        rsa.initiateOrder(revenueToken, 1, 0, block.timestamp + 100 days);
+        startPrank(lender);
+        rsa.deposit();
+    }
+
+    function invariant_initiateOrder_cantSellCreditToken() public {
+        startPrank(lender);
+        rsa.deposit();
+        vm.expectRevert("Cant sell token beingbought");
+        rsa.initiateOrder(creditToken, 1, 0, block.timestamp + 100 days);
+    }
+
+    function test_initiateOrder_lenderOrBorrowerCanSubmit() public {
+        
+    }
+
+    /*********************
+    **********************
+    
+    EIP-2981 Order Verification
+    
+    **********************
+    *********************/
+
+    function invariant_verifySignature_mustUseERC20Balance() public {
+        revert();
+    }
+
+    function invariant_verifySignature_mustBuyCreditToken() public {
+        revert();
+    }
+
+    function invariant_verifySignature_mustBeSellOrder() public {
+        revert();
+    }
+
+    function invariant_verifySignature_mustSignOrderFromCowContract() public {
+        revert();
+    }
+
+
+    /**
+     * @dev Creates a new Revenue Share Agreement mints token to lender and approves to RSA
+    * @param _spigot address of spigot to use for revenue share
+    * @param _token address of token being lent
+    * @param _lender address that will lend to RSA
+    * @param _initialPrincipal amount of tokens to lend
+    * @param _totalOwed total amount of tokens owed to lender through RSA
+     */
+    function _initRSA(
+        address _spigot,
+        address _token,
+        address _lender,
+        uint256 _initialPrincipal,
+        uint256 _totalOwed
+    ) internal returns(RSA newRSA) {
+        newRSA = new RSA(
+            _spigot,
+            borrower,
+            _token,
+            lenderRevenueSplit,
+            _initialPrincipal,
+            _totalOwed,
+            "RSA Revenue Stream Token",
+            "rsaCLAIM"
+        );
+
+        creditToken.mint(_initialPrincipal, _lender);
+        vm.startPrank(_lender);
+        creditToken.approve(address(newRSA), tyoe(uint256).max);
     }
 
     /**
@@ -145,5 +267,4 @@ contract SpigotTest is Test {
             'Invalid treasury payment amount for spigot revenue'
         );
     }
-
 }
